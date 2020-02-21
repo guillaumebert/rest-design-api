4ffbfea2dfacb5410d264c59da10c7475a37ec66
Guillaume BERT <guillaume.bert@neotys.com>
2020-02-21 16:22:38 +0100

    Updated lib : Simple.OData.Client Use new version of Neoload-rest-common-api

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Neotys")]
 [assembly: AssemblyProduct("NeotysDesignAPI")]
-[assembly: AssemblyCopyright("Copyright © Neotys 2018")]
+[assembly: AssemblyCopyright("Copyright © Neotys 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b532b87e-82a6-402e-874f-df3f21d656ad")]
+[assembly: Guid("EBE002AD-8778-49E0-9355-A01941374071")]
 
 // Version information for an assembly consists of the following four values:
 //
